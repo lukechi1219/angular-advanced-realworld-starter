625dab52ff86a218b10a57590e18d15129cebf50
luke.chi <luke@neutec.com.tw>
2021-09-04 12:30:17 +0800

    fix bug

--- src/app/posts/posts-routing.module.ts
+++ src/app/posts/posts-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateComponent } from './create/create.component';
+import { PostComponent } from './post/post.component';
 import { PostsComponent } from './posts.component';
 
 const routes: Routes = [
@@ -15,7 +16,7 @@ const routes: Routes = [
   },
   {
     path: 'posts/:id',
-    component: PostsComponent,
+    component: PostComponent,
   },
   {
     path: 'create',
